d3c76892589077682d4570e97071c2d14afed426
Oleg Gordienov <ogordienov@yandex.ru>
2022-01-15 18:19:13 +0200

    fix: delete js comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// ищем DOM-элементы по классам
-
 const popup = document.querySelector('.popup');
 const popupForm = popup.querySelector('.popup__form_edit-profile');
 const profileEditButton = document.querySelector('.profile__edit-button');
@@ -9,8 +7,7 @@ const descriptionInput = popup.querySelector('.popup__text-input_description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
-function editButtonHandler(evt) {
-  evt.preventDefault(); // т.к. элемент сделан ссылкой для правильной семантики и доступности, необходимо отключить поведение по умолчанию (о чем собственно было сказано в эталонной работе на прошлом вебинаре)
+function editButtonHandler() {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
