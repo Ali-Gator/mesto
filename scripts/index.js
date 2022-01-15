a70fc5ca3614b7f900a677ef430a86278dfea632
Oleg Gordienov <ogordienov@yandex.ru>
2022-01-15 17:42:50 +0200

    hotfixes: small issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,32 @@
+// ищем DOM-элементы по классам
+
 const popup = document.querySelector('.popup');
-const popupForm = popup.querySelector('.popup__container');
+const popupForm = popup.querySelector('.popup__form_edit-profile');
 const profileEditButton = document.querySelector('.profile__edit-button');
+const popupCloseButton = popupForm.querySelector('.popup__close-button');
+const nameInput = popup.querySelector('.popup__text-input_name');
+const descriptionInput = popup.querySelector('.popup__text-input_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
 
 function editButtonHandler(evt) {
-  const popupCloseButton = popup.querySelector('.popup__close-button');
-  const nameInput = popup.querySelector('.popup__text-input[name="username"]');
-  const descriptionInput = popup.querySelector('.popup__text-input[name="description"]');
-  const profileName = document.querySelector('.profile__name');
-  const profileDescription = document.querySelector('.profile__description');
-
-  evt.preventDefault();
+  evt.preventDefault(); // т.к. элемент сделан ссылкой для правильной семантики и доступности, необходимо отключить поведение по умолчанию (о чем собственно было сказано в эталонной работе на прошлом вебинаре)
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
+}
 
-  function closePopupHandler(evt) {
-    if (evt.target === evt.currentTarget || evt.target === popupCloseButton) {
-      popup.classList.remove('popup_opened');
-    }
-  }
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 
-  popup.addEventListener('click', closePopupHandler);
-
-  function saveButtonHandler(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileDescription.textContent = descriptionInput.value;
-    popup.classList.remove('popup_opened');
-  }
-
-  popupForm.addEventListener('submit', saveButtonHandler);
+function saveButtonHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closePopup();
 }
 
 profileEditButton.addEventListener('click', editButtonHandler);
+popupCloseButton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', saveButtonHandler);
